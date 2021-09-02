9c570a98292790210a6f4fedb3b0fb1a08f9591e
acyadigar <alpercanyadigar@gmail.com>
2021-09-02 23:29:17 +0300

    Change the status code of login error

--- backend/controllers/auth/login.js
+++ backend/controllers/auth/login.js
@@ -3,10 +3,10 @@ const UserService = require('../../services/user-service')
 
 const login = async (req, res) => {
   const validUser = await UserService.findByUsername(req.body.username)
-  if(!validUser) return res.status(404).send('Username or password is not correct!')
+  if(!validUser) return res.status(403).send('Username or password is not correct!')
 
   const user = await AuthService.login(req.body, validUser)
-  if(!user) return res.status(404).send('Username or password is not correct!')
+  if(!user) return res.status(403).send('Username or password is not correct!')
 
   res.send(user)
 }
